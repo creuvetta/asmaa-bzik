604f65a635662cd8bb9ce8fea007a8d84b6d102e
creuvetta <asmaabzik25@gmail.com>
2025-04-03 23:51:10 +0000

    Update profil-page.dart

--- TP2/lib/screens/profil-page.dart
+++ TP2/lib/screens/profil-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -6,9 +7,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profil'),
-      ),
+      appBar: AppBar(title: const Text('Profil')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -18,12 +17,21 @@ class ProfilePage extends StatelessWidget {
               child: Icon(Icons.person, size: 50),
             ),
             const SizedBox(height: 20),
-            const Text('Utilisateur', style: TextStyle(fontSize: 24)),
-            const SizedBox(height: 10),
-            const Text('user@example.com'),
+            FutureBuilder(
+              future: AuthService.getUserId(),
+              builder: (context, snapshot) {
+                return Text(
+                  'Utilisateur #${snapshot.data ?? 'inconnu'}',
+                  style: const TextStyle(fontSize: 24),
+                );
+              },
+            ),
             const SizedBox(height: 30),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                await AuthService.logout();
+                Navigator.pushReplacementNamed(context, '/login');
+              },
               child: const Text('Déconnexion'),
             ),
           ],
