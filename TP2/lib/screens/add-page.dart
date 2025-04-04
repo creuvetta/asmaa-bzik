4744260e08ecacc642af3dfe5c66286f624f2070
creuvetta <asmaabzik25@gmail.com>
2025-04-04 09:40:42 +0000

    Update add-page.dart

--- TP2/lib/screens/add-page.dart
+++ TP2/lib/screens/add-page.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import '../services/api_service.dart';
+import '../services/api-service.dart';
 
 class AddPage extends StatefulWidget {
   const AddPage({super.key});
